4546169f1852a8d4207d3e0e983babfc33921dda
Katherine <sinc0115@algonquinlive.com>
2020-09-20 11:13:45 -0400

    Create contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,15 @@
+ext = false
+
+// ADDFILTER()
+function addFilter() {
+    if (ext == false) {
+        document.body.style.filter = "grayscale(100%)"
+        ext = true
+    } else {
+        document.body.style.filter = "none"
+        ext = false
+    }
+}
+
+// ENABLE FILTER
+browser.runtime.onConnect.addListener(addFilter)
