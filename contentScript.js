81330b1251b1ca922bbacff5d4be14fdf8da13f1
Katherine Sinclair <sinc0115@algonquinlive.com>
2020-10-17 14:49:43 -0400

    Fixes display, removes function

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,15 @@
-ext = false
+// ext = false
 
 // ADDFILTER()
-function addFilter() {
-    if (ext == false) {
-        document.body.style.filter = "grayscale(100%)"
-        ext = true
-    } else {
-        document.body.style.filter = "none"
-        ext = false
-    }
-}
+// function addFilter () {
+//   if (ext == false) {
+document.body.style.filter = 'grayscale(100%)'
+//     ext = true
+//   } else {
+//     document.body.style.filter = 'none'
+//     ext = false
+//   }
+// }
 
 // ENABLE FILTER
-browser.runtime.onConnect.addListener(addFilter)
+// browser.runtime.onConnect.addListener(addFilter)
